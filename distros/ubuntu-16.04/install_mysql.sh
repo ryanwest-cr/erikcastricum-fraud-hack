e515cfd5831912125ca4c8393762f1056850cbab
Erik Castricum <git@cas-online.nl>
2017-07-01 14:19:25 +0200

    ubuntu 16.04: fix mariadb security
    follow https://github.com/servisys/ispconfig_setup/commit/8e1f845c9a136ac5564c28605ce41e143932e990
    
    Signed-off-by: Erik Castricum <git@cas-online.nl>

--- distros/ubuntu-16.04/install_mysql.sh
+++ distros/ubuntu-16.04/install_mysql.sh
@@ -20,6 +20,18 @@ InstallSQLServer() {
     #echo "mariadb-server-10.0 mysql-server/root_password_again password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
 	apt-get -yqq install mariadb-client mariadb-server > /dev/null 2>&1
     sed -i 's/bind-address		= 127.0.0.1/#bind-address		= 127.0.0.1/' /etc/mysql/mariadb.conf.d/50-server.cnf
+    echo "update mysql.user set plugin = 'mysql_native_password' where user='root';" | mysql -u root
+	sed -i 's/password =/password = '$CFG_MYSQL_ROOT_PWD'/' /etc/mysql/debian.cnf
+	mysql -e "UPDATE mysql.user SET Password = PASSWORD('$CFG_MYSQL_ROOT_PWD') WHERE User = 'root'"
+	echo -n "Fix Mysql security"
+	# Kill the anonymous users
+	mysql -e "DROP USER ''@'localhost'"
+	# Because our hostname varies we'll use some Bash magic here.
+	mysql -e "DROP USER ''@'$(hostname)'"
+	# Kill off the demo database
+	mysql -e "DROP DATABASE test"
+	# Make our changes take effect
+	mysql -e "FLUSH PRIVILEGES"
     service mysql restart /dev/null 2>&1
     echo -e "[${green}DONE${NC}]\n"
   fi	

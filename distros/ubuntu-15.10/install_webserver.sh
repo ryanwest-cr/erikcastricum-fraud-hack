3bbb062d7e2a107489b4b0161ebe0bea53a5dbde
Erik Castricum <git@cas-online.nl>
2017-07-17 21:43:21 +0200

    we do it in Amsterdam :P
    Signed-off-by: Erik Castricum <git@cas-online.nl>

--- distros/ubuntu-15.10/install_webserver.sh
+++ distros/ubuntu-15.10/install_webserver.sh
@@ -60,7 +60,7 @@ InstallWebServer() {
 	service nginx start 
 	apt-get -yqq install php5-fpm php5-mysqlnd php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-memcached php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl memcached php-apc > /dev/null 2>&1
 	sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php5/fpm/php.ini
-	sed -i "s/;date.timezone =/date.timezone=\"Europe\/Rome\"/" /etc/php5/fpm/php.ini
+	sed -i "s/;date.timezone =/date.timezone=\"Europe\/Amsterdam\"/" /etc/php5/fpm/php.ini
 	#sed -i "s/#/;/" /etc/php5/conf.d/ming.ini
 	service php5-fpm reload
 	apt-get -yqq install fcgiwrap

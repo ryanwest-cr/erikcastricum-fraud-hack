2086503e343d47e57e17ba347f64b79c47d82b52
Erik Castricum <git@cas-online.nl>
2022-04-23 13:05:41 +0200

    ubuntu 22.04: drop tinymce
    not available
    
    Signed-off-by: Erik Castricum <git@cas-online.nl>

--- distros/ubuntu-22.04/install_webmail.sh
+++ distros/ubuntu-22.04/install_webmail.sh
@@ -12,7 +12,7 @@ InstallWebmail() {
 	  echo "roundcube-core roundcube/mysql/app-pass password $CFG_ROUNDCUBE_PWD" | debconf-set-selections
 	  echo "roundcube-core roundcube/app-password-confirm password $CFG_ROUNDCUBE_PWD" | debconf-set-selections
 	  echo "roundcube-core roundcube/hosts string localhost" | debconf-set-selections
-	  apt_install roundcube roundcube-core roundcube-mysql roundcube-plugins javascript-common libjs-jquery-mousewheel php-net-sieve tinymce
+	  apt_install roundcube roundcube-core roundcube-mysql roundcube-plugins javascript-common libjs-jquery-mousewheel php-net-sieve
 	  sed -i "s/\$config\['default_host'\] = '';/\$config['default_host'] = 'localhost';/" /etc/roundcube/config.inc.php
 	  if [ "$CFG_WEBSERVER" == "apache" ]; then
 		echo "Alias /webmail /var/lib/roundcube" >> /etc/apache2/conf-enabled/roundcube.conf

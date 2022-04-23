2b4eb848346d9c49b65270722f081d367bf9b076
Erik Castricum <git@cas-online.nl>
2022-04-23 13:20:42 +0200

    ubuntu22.04: getmail: getmail4 --> getmail6
    Signed-off-by: Erik Castricum <git@cas-online.nl>

--- distros/ubuntu-22.04/install_postfix.sh
+++ distros/ubuntu-22.04/install_postfix.sh
@@ -14,7 +14,7 @@ InstallPostfix() {
   echo -n "Installing SMTP Mail server (Postfix)... "
   echo "postfix postfix/main_mailer_type select Internet Site" | debconf-set-selections
   echo "postfix postfix/mailname string $CFG_HOSTNAME_FQDN" | debconf-set-selections
-  apt_install postfix postfix-mysql postfix-doc getmail4
+  apt_install postfix postfix-mysql postfix-doc getmail6
   sed -i "s/#submission inet n       -       -       -       -       smtpd/submission inet n       -       -       -       -       smtpd/" /etc/postfix/master.cf
   sed -i "s/#  -o syslog_name=postfix\/submission/  -o syslog_name=postfix\/submission/" /etc/postfix/master.cf
   sed -i "s/#  -o smtpd_tls_security_level=encrypt/  -o smtpd_tls_security_level=encrypt/" /etc/postfix/master.cf
